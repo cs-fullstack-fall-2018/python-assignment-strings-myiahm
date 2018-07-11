11bc7325ca5df6994176c3ff4350b59e3e2f7ef1
Myiah Moore <myiahsade@gmail.com>
2018-07-11 15:36:07 -0500

    check in

---
+++ Example1.py
@@ -0,0 +1,26 @@
+import os
+
+os.system('clear')
+
+
+bandMate ="riNgo"
+print ("The name is this long " , len(bandMate))
+print (bandMate.upper())
+print (bandMate.lower())
+print (bandMate[2])
+print (bandMate[1:4])
+print (bandMate[3:4])
+
+print (bandMate.capitalize())
+print (bandMate.swapcase())
+name =input("Enter name: ")
+namelength = len(name)
+print(namelength)
+name = input("Enter name1:" )
+print(namelength)
+name = input("Enter name2: ")
+print(namelength)
+name = input("Enter name3: ")
+print(namelength)
+
+if 
